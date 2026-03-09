6a849bbc386dc243f6dbeaaed5e9d8c9080da8f1
Dan Frunza <dani.frunza@yahoo.com>
2026-03-09 14:24:30 +0200

    Small ajustment

--- co2-simulation/co2-simulation.py
+++ co2-simulation/co2-simulation.py
@@ -2,6 +2,7 @@ import time
 import random
 import paho.mqtt.client as mqtt
 
+
 BROKER = "mqtt-local"
 PORT = 1883
 
@@ -9,37 +10,35 @@ client = mqtt.Client()
 client.connect(BROKER, PORT, 60)
 
 co2 = 700
-purifier_on = False
+ventilation_on = False
 
 # parameters for CO2 simulation
-num_people = 1  # number of people
-ventilation = False  # ventilation active or not
+num_people = 2  # number of people
+
 
 
 def on_message(client, userdata, msg):
-    global purifier_on
+    global ventilation_on
     if msg.payload.decode() == "ON":
-        purifier_on = True
+        ventilation_on = True
     elif msg.payload.decode() == "OFF":
-        purifier_on = False
+        ventilation_on = False
 
 
-client.subscribe("home/purifier/control")
+
+client.subscribe("home/ventilation/control")
 client.on_message = on_message
 
 client.loop_start()
 
 while True:
-    if purifier_on:
-        co2 -= random.randint(10, 20)  # purificator reduce CO2
-        if co2 < 400:  # minimum CO2 level
-            co2 = 400
+    if ventilation_on:
+        co2 -= random.randint(10, 20)  # ventilation reduce CO2
+        if co2 < 700:  # minimum CO2 level
+            co2 = 700
     else:
         increase = num_people * random.choices([0, 1], weights=[0.5, 0.5])[0]
-        if not ventilation:
-            co2 += increase
-        else:
-            co2 += max(0, increase - random.randint(1, 2))  # ventilation reduces CO2 increase
+        co2 += increase
 
     client.publish("home/co2", co2)
     time.sleep(15)
